85377d24c7e7beab89124a565af3c7781f023c54
jokzcooks <jokzcooks@gmail.com>
2024-09-14 17:38:28 -0400

    Infra

--- MainApp/client/src/containers/Base/index.jsx
+++ MainApp/client/src/containers/Base/index.jsx
@@ -1,5 +1,7 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import HomePage from '../Home';
+import Header from '../../components/Header';
 
 const Base = () => {
   const navigate = useNavigate();
@@ -13,7 +15,9 @@ const Base = () => {
 
   return (
     <div className='mainContainer'>
+        <Header />
         <Routes>
+            <Route path='/home' element={<HomePage />} />
         </Routes>
     </div>
   );
