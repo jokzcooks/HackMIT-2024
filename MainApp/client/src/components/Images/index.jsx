c4585376e50c8649c39823656396e2055e589c8a
KANIKA GUPTA <85227915+KanikaGupta16@users.noreply.github.com>
2024-09-15 10:03:27 -0400

    Update index.jsx

--- MainApp/client/src/components/Images/index.jsx
+++ MainApp/client/src/components/Images/index.jsx
@@ -14,3 +14,24 @@ export {
     default as ResumeIcon
 }
 from './icons/resume.svg'
+export {
+    default as ProfileIcon1
+}
+from './icons/magicstudio-art.webp'
+export {
+    default as ProfileIcon2
+}
+from './icons/magicstudio1-art.webp'
+export {
+    default as ProfileIcon3
+}
+from './icons/magicstudio-art2.webp'
+export {
+    default as ProfileIcon4
+}
+from './icons/magicstudio-art3.webp'
+
+export {
+    default as ProfileIcon5
+}
+from './icons/magicstudio-art4.webp'
